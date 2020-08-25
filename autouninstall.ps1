a9eae15bb96f1fa4cd8be09f3f7e10350bee576b
hackofalltrades <gkinger@gmail.com>
2020-08-24 21:53:57 -0500

    Update autouninstall.ps1

--- autouninstall.ps1
+++ autouninstall.ps1
@@ -1,5 +1,5 @@
+### Usage autouninstall.ps1 -appname "Apache Tomcat"
 ### Use the below line to list all apps to determine how the app is named in the registry.
-
 ### Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Select-Object DisplayName
 param ($appname)
 
